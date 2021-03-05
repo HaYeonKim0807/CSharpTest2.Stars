1cd9ed7a860de7af35d6866d260d9e8c6b50b81b
HaYeonKim0807 <kh701201@naver.com>
2021-03-06 05:13:21 +0900

    Hello

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int a = 1; a <= 5; a++ )
+            {
+                
+                for (int b = 1; b <= a; b++ )
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+    }
+}
